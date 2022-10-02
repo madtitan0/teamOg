570608aa6fc1073abc5b17de02ab5bc4a4c9ece5
Muhammed Riyaz <muhammedriyaz@Muhammeds-MacBook-Air.local>
2022-10-02 08:42:29 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/elderlearnapp/generatedsplashanimatewidget/GeneratedSplashanimateWidget.dart';
+import 'package:flutterapp/elderlearnapp/generatedsplashwidget/GeneratedSplashWidget.dart';
+import 'package:flutterapp/elderlearnapp/generatedloginanimate1widget/GeneratedLoginanimate1Widget.dart';
+import 'package:flutterapp/elderlearnapp/generatedloginanimate2widget/GeneratedLoginanimate2Widget.dart';
+import 'package:flutterapp/elderlearnapp/generatedloginanimate3widget/GeneratedLoginanimate3Widget.dart';
+import 'package:flutterapp/elderlearnapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimatewidget/GeneratedDashboardanimateWidget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimatewidget1/GeneratedDashboardanimateWidget1.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget/GeneratedDashboardanimate00Widget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget1/GeneratedDashboardanimate00Widget1.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget2/GeneratedDashboardanimate00Widget2.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget3/GeneratedDashboardanimate00Widget3.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget4/GeneratedDashboardanimate00Widget4.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget5/GeneratedDashboardanimate00Widget5.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget6/GeneratedDashboardanimate00Widget6.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget7/GeneratedDashboardanimate00Widget7.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate00widget8/GeneratedDashboardanimate00Widget8.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate03widget/GeneratedDashboardanimate03Widget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate03widget1/GeneratedDashboardanimate03Widget1.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate03widget2/GeneratedDashboardanimate03Widget2.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate01widget/GeneratedDashboardanimate01Widget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate01widget1/GeneratedDashboardanimate01Widget1.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate01widget2/GeneratedDashboardanimate01Widget2.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate02widget/GeneratedDashboardanimate02Widget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate02widget1/GeneratedDashboardanimate02Widget1.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate02widget2/GeneratedDashboardanimate02Widget2.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardwidget/GeneratedDashboardWidget.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardwidget1/GeneratedDashboardWidget1.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardwidget2/GeneratedDashboardWidget2.dart';
+import 'package:flutterapp/elderlearnapp/generatedanmialanimatewidget/GeneratedAnmialanimateWidget.dart';
+import 'package:flutterapp/elderlearnapp/generatedanmialwidget/GeneratedAnmialWidget.dart';
+import 'package:flutterapp/elderlearnapp/generatedbuttonswidget25/GeneratedButtonsWidget25.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimate1widget/GeneratedDashboardanimate1Widget.dart';
+import 'package:flutterapp/elderlearnapp/generatedframe9widget/GeneratedFrame9Widget.dart';
+import 'package:flutterapp/elderlearnapp/generatedgroup1widget4/GeneratedGroup1Widget4.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimatewidget2/GeneratedDashboardanimateWidget2.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimatewidget3/GeneratedDashboardanimateWidget3.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardanimatewidget4/GeneratedDashboardanimateWidget4.dart';
+import 'package:flutterapp/elderlearnapp/generatedframe8widget21/GeneratedFrame8Widget21.dart';
+import 'package:flutterapp/elderlearnapp/generatedbufferedwidget14/GeneratedBufferedWidget14.dart';
+import 'package:flutterapp/elderlearnapp/generateddashboardwidget3/GeneratedDashboardWidget3.dart';
+import 'package:flutterapp/elderlearnapp/generatedwidget24/GeneratedWidget24.dart';
+import 'package:flutterapp/elderlearnapp/generatedbuttonswidget31/GeneratedButtonsWidget31.dart';
+
+void main() {
+  runApp(ElderlearnApp());
+}
+
+class ElderlearnApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedSplashWidget',
+      routes: {
+        '/GeneratedSplashanimateWidget': (context) =>
+            GeneratedSplashanimateWidget(),
+        '/GeneratedSplashWidget': (context) => GeneratedSplashWidget(),
+        '/GeneratedLoginanimate1Widget': (context) =>
+            GeneratedLoginanimate1Widget(),
+        '/GeneratedLoginanimate2Widget': (context) =>
+            GeneratedLoginanimate2Widget(),
+        '/GeneratedLoginanimate3Widget': (context) =>
+            GeneratedLoginanimate3Widget(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedDashboardanimateWidget': (context) =>
+            GeneratedDashboardanimateWidget(),
+        '/GeneratedDashboardanimateWidget1': (context) =>
+            GeneratedDashboardanimateWidget1(),
+        '/GeneratedDashboardanimate00Widget': (context) =>
+            GeneratedDashboardanimate00Widget(),
+        '/GeneratedDashboardanimate00Widget1': (context) =>
+            GeneratedDashboardanimate00Widget1(),
+        '/GeneratedDashboardanimate00Widget2': (context) =>
+            GeneratedDashboardanimate00Widget2(),
+        '/GeneratedDashboardanimate00Widget3': (context) =>
+            GeneratedDashboardanimate00Widget3(),
+        '/GeneratedDashboardanimate00Widget4': (context) =>
+            GeneratedDashboardanimate00Widget4(),
+        '/GeneratedDashboardanimate00Widget5': (context) =>
+            GeneratedDashboardanimate00Widget5(),
+        '/GeneratedDashboardanimate00Widget6': (context) =>
+            GeneratedDashboardanimate00Widget6(),
+        '/GeneratedDashboardanimate00Widget7': (context) =>
+            GeneratedDashboardanimate00Widget7(),
+        '/GeneratedDashboardanimate00Widget8': (context) =>
+            GeneratedDashboardanimate00Widget8(),
+        '/GeneratedDashboardanimate03Widget': (context) =>
+            GeneratedDashboardanimate03Widget(),
+        '/GeneratedDashboardanimate03Widget1': (context) =>
+            GeneratedDashboardanimate03Widget1(),
+        '/GeneratedDashboardanimate03Widget2': (context) =>
+            GeneratedDashboardanimate03Widget2(),
+        '/GeneratedDashboardanimate01Widget': (context) =>
+            GeneratedDashboardanimate01Widget(),
+        '/GeneratedDashboardanimate01Widget1': (context) =>
+            GeneratedDashboardanimate01Widget1(),
+        '/GeneratedDashboardanimate01Widget2': (context) =>
+            GeneratedDashboardanimate01Widget2(),
+        '/GeneratedDashboardanimate02Widget': (context) =>
+            GeneratedDashboardanimate02Widget(),
+        '/GeneratedDashboardanimate02Widget1': (context) =>
+            GeneratedDashboardanimate02Widget1(),
+        '/GeneratedDashboardanimate02Widget2': (context) =>
+            GeneratedDashboardanimate02Widget2(),
+        '/GeneratedDashboardWidget': (context) => GeneratedDashboardWidget(),
+        '/GeneratedDashboardWidget1': (context) => GeneratedDashboardWidget1(),
+        '/GeneratedDashboardWidget2': (context) => GeneratedDashboardWidget2(),
+        '/GeneratedAnmialanimateWidget': (context) =>
+            GeneratedAnmialanimateWidget(),
+        '/GeneratedAnmialWidget': (context) => GeneratedAnmialWidget(),
+        '/GeneratedButtonsWidget25': (context) => GeneratedButtonsWidget25(),
+        '/GeneratedDashboardanimate1Widget': (context) =>
+            GeneratedDashboardanimate1Widget(),
+        '/GeneratedFrame9Widget': (context) => GeneratedFrame9Widget(),
+        '/GeneratedGroup1Widget4': (context) => GeneratedGroup1Widget4(),
+        '/GeneratedDashboardanimateWidget2': (context) =>
+            GeneratedDashboardanimateWidget2(),
+        '/GeneratedDashboardanimateWidget3': (context) =>
+            GeneratedDashboardanimateWidget3(),
+        '/GeneratedDashboardanimateWidget4': (context) =>
+            GeneratedDashboardanimateWidget4(),
+        '/GeneratedFrame8Widget21': (context) => GeneratedFrame8Widget21(),
+        '/GeneratedBufferedWidget14': (context) => GeneratedBufferedWidget14(),
+        '/GeneratedDashboardWidget3': (context) => GeneratedDashboardWidget3(),
+        '/GeneratedWidget24': (context) => GeneratedWidget24(),
+        '/GeneratedButtonsWidget31': (context) => GeneratedButtonsWidget31(),
+      },
+    );
+  }
+}
